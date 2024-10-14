40bb4c568aac2d9e949e35a2d70468ec5a29b3ae
AliveTube <92478946+AliveTube@users.noreply.github.com>
2024-10-15 00:11:09 +0300

    Added routes and controllers for tasks and user

--- Task-Management-System/Server/controllers/taskController.js
+++ Task-Management-System/Server/controllers/taskController.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //TODO specify the actuall used db 
 const connectDB = require('../config/db');  // Import the database connection
 
@@ -101,5 +102,57 @@ exports.addTaskToTodoList = async (req, res) => {
     } catch (error) {
         console.error('Error adding task to to-do list:', error);
         res.status(500).json({ message: 'Error adding task to to-do list' });
+=======
+const db = require('../config/dbConnection');
+
+exports.getAllTasks = (req, res) => {
+    try {
+        db.query('SELECT * FROM Tasks', (err, data) => {
+            if (err) {
+                return res.status(500).send('Server Error');
+            }
+            res.json(data);
+        });
+    } catch (err) {
+        console.error('Error fetching tasks:', err.message);
+        res.status(500).send('Server Error');
+    }
+};
+
+exports.getTaskById = (req, res) => {
+    try {
+        const { uniqueId } = req.params;
+        db.query('SELECT * FROM Tasks WHERE uniqueId = ?', [uniqueId], (err, data) => {
+            if (err) {
+                return res.status(500).send('Server Error');
+            }
+            if (!data.length) {
+                return res.status(404).json({ message: 'Task not found' });
+            }
+            res.json(data[0]);
+        });
+
+    } catch (err) {
+        console.error('Error fetching task:', err.message);
+        res.status(500).send('Server Error');
+    }
+};
+
+exports.updateTaskStatus = (req, res) => {
+    try {
+        const { uniqueId } = req.params;
+        const { status } = req.body;
+
+        db.query('UPDATE Tasks SET status = ? WHERE uniqueId = ?', [status, uniqueId], (err, result) => {
+            if (err) {
+                return res.status(500).send('Server Error');
+            }
+        });
+        res.status(200).send('Task status updated successfully');
+    }
+    catch (err) {
+        console.error('Error updating task status:', err.message);
+        res.status(500).send('Server Error');
+>>>>>>> d2fa509 (Added routes and controllers for tasks and user)
     }
 };
